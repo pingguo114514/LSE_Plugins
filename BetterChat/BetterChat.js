4dd3e0c82aec910027eb3e78e0a9fe06eb3941f8
pingguo114514 <1135636981@qq.com>
2025-01-12 19:25:16 +0800

    add BetterChat&fix some bug

---
+++ BetterChat/BetterChat.js
@@ -0,0 +1,66 @@
+// LiteLoader-AIDS automatic generated
+/// <reference path="c:\LSE/dts/HelperLib-master/src/index.d.ts"/> 
+
+var conf = new JsonConfigFile('plugins/BetterChat/config.json');
+conf.init('showPlatform', true);
+conf.init('showGmode', true);
+conf.init('showDimension', true);
+conf.init('showOrganization', true);
+conf.init('showLatency', true);
+conf.init('badWordsFilter', true);
+conf.init('sparkbridge2', false);
+var language = new JsonConfigFile('plugins/BetterChat/language.json', JSON.stringify({
+    device: {
+        "Android": "安卓",
+        "Google": "安卓",
+        "iOS": "iOS",
+        "OSX": "MacOS",
+        "Amazon": "平板/电视亚马逊FireOS",
+        "GearVR": "VR",
+        "Hololens": "VR",
+        "Windows10": "电脑",
+        "Win32": "电脑",
+        "TVOS": "TVOS",
+        "PlayStation": "PS",
+        "Nintendo": "Switch",
+        "Xbox": "Xbox",
+        "WindowsPhone": "WindowsPhone",
+        "Unknown": "未知设备"
+    },
+    gamemode: {
+        "0": "生存",
+        "1": "创造",
+        "2": "冒险",
+        "6": "旁观"
+    },
+    dimension: {
+        "0": "主世界",
+        "1": "地狱",
+        "2": "末地"
+    }
+}));
+if (conf.get('badWordsFilter')) var check = ll.imports('ProhibitedWords', 'check'), replace = ll.imports('ProhibitedWords', 'replace');
+if (conf.get('showOrganization')) var orgNameQuery = ll.imports('orgEX', 'orgEX_getPlayerOrgName');
+if (conf.get('sparkbridge2')) var send = ll.imports('SparkAPI', 'sendGroupMessage');
+mc.listen('onChat', (pl, msg) => {
+    if (conf.get('badWordsFilter') && check(msg)) {
+        pl.tell('§c§l你的消息包含敏感词汇');
+        msg = replace(msg, '*');
+    }
+    let t = [], dv = pl.getDevice();
+    let latency = dv.lastPing;
+    let orgName = (ll.hasExported('orgEX', 'orgEX_getPlayerOrgName') ? orgNameQuery(pl.xuid) : 'orgEX未安装');
+    if (latency <= 100) latency = '§a' + latency;
+    else if (latency <= 400) latency = '§e' + latency;
+    else latency = '§c' + latency;
+    if (conf.get('showPlatform')) t.push(`§a${language.get('device')[(dv.os in language.get('device')) ? dv.os : 'Unknown']}`);
+    if (conf.get('showDimension')) t.push(`§6${language.get('dimension')[pl.pos.dimid]}`);
+    if (conf.get('showGmode')) t.push(`§b${language.get('gamemode')[pl.gameMode]}`);
+    if (conf.get('showLatency')) t.push(`${latency}ms`);
+    if (conf.get('showOrganization')) t.push(`§d${orgName}`);
+    let preMsg = `§r[${t.join('§r|')}§r]<${pl.name}>`;
+    mc.broadcast(preMsg + msg);
+    if (conf.get('sparkbridge2')) send(`${pl.name}:${msg}`);
+    // logger.info(preMsg + msg);
+    return false;
+});
--- ProhibitedWords/ProhibitedWords.js
+++ ProhibitedWords/ProhibitedWords.js
@@ -1,29 +1,73 @@
 // LiteLoader-AIDS automatic generated
 /// <reference path="c:\LSE/dts/HelperLib-master/src/index.d.ts"/> 
-var conf = new JsonConfigFile('plugins\\ProhibitedWords\\config.json'), list, regexlist;
-function loadConfig() {
-    list = conf.init('list', []);
-    regexlist = conf.init('regexlist', []);
-}
 
+var conf = new JsonConfigFile('plugins\\ProhibitedWords\\config.json'), list, regexlist, caseSensitive;
+list = conf.init('list', []);
+regexlist = conf.init('regexlist', []);
+caseSensitive = conf.init('caseSensitive', true);
 function check(text) {
     for (let i of list) {
-        if (text.includes(i)) {
+        if (caseSensitive ? text.includes(i) : text.toLowerCase().includes(i.toLowerCase())) {
             return true;
         }
     }
     for (let i of regexlist) {
-        if (new RegExp(i).test(text)) {
+        if (new RegExp(i, caseSensitive ? '' : 'i').test(text)) {
             return true;
         }
     }
     return false;
 }
-function getlist(){
+function getlist() {
     return {
         list: list,
         regexlist: regexlist
     }
 }
-ll.exports(check,'ProhibitedWords', 'check');
-ll.exports(getlist,'ProhibitedWords', 'getlist');
+function generateStars(i, f) {
+    return f.repeat(i);
+}
+function replaceCaseInsensitive(str, target, replacement) {
+    let result = '';
+    let currentIndex = 0;
+    let lowercaseStr = str.toLowerCase();
+    let lowercaseTarget = target.toLowerCase();
+    let targetLength = target.length;
+    while (true) {
+        let index = lowercaseStr.indexOf(lowercaseTarget, currentIndex);
+        if (index === -1) {
+            result += str.substring(currentIndex);
+            break;
+        }
+        result += str.substring(currentIndex, index) + replacement;
+        currentIndex = index + targetLength;
+    }
+    return result;
+}
+function replace(text, f) {
+    if (caseSensitive) {
+        regexlist.forEach(i => {
+            let reg = new RegExp(i, 'g');
+            text = text.replace(reg, match => {
+                return generateStars(match.length, f);
+            });
+        });
+        list.forEach(w => {
+            text = text.replace(w, generateStars(w.length, f));
+        });
+    } else {
+        regexlist.forEach(i => {
+            let reg = new RegExp(i, 'gi');
+            text = text.replace(reg, match => {
+                return generateStars(match.length, f);
+            });
+        });
+        list.forEach(w => {
+            text = replaceCaseInsensitive(text, w, generateStars(w.length, f));
+        });
+    }
+    return text;
+}
+ll.exports(check, 'ProhibitedWords', 'check');
+ll.exports(getlist, 'ProhibitedWords', 'getlist');
+ll.exports(replace, 'ProhibitedWords', 'replace');
