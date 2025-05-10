2d945b2c6b7fb2d27d3569ccd53bc38e1422ecc3
pingguo114514 <1135636981@qq.com>
2025-05-10 20:15:18 +0800

    refactor(iLandFly): 优化飞行检测机制并添加配置支持
    - 支持配置检查间隔
    
    - 从 onTick 事件更改为 setInterval 定时检查，减少服务器负担

--- iLandFly/iLandFly.js
+++ iLandFly/iLandFly.js
@@ -1,36 +1,32 @@
 // LiteLoader-AIDS automatic generated
 /// <reference path="c:\LSE/dts/HelperLib-master/src/index.d.ts"/> 
 
-ll.registerPlugin(
-    /* name */ "iLandFly",
-    /* introduction */ "领地飞行",
-    /* version */ [1,0,0]
-); 
-var trust,landowner,getland;
-function FlyOn(pl){
+function FlyOn(pl) {
     pl.setAbility(10, true);
     pl.tell('您已进入领地，飞行已开启');
 }
-function FlyOff(pl){
+function FlyOff(pl) {
     pl.setAbility(10, false);
     pl.tell('您已离开领地，飞行已关闭');
 }
-mc.listen('onServerStarted',()=>{
-    trust=ll.import('ILAPI_IsPlayerTrusted');
-    landowner=ll.import('ILAPI_IsLandOwner');
-    getland=ll.import('ILAPI_PosGetLand');
-    logger.info('已加载');
-});
-mc.listen('onTick',()=>{
-    mc.getOnlinePlayers().forEach(pl=>{
-        if(pl.isCreative||pl.isSpectator) return;
-        let landid=getland(pl.pos);
-        if(pl.canFly){
-            if(landid==-1) return FlyOff(pl);
-            if(!(trust(landid,pl.xuid)||landowner(landid,pl.xuid))) FlyOff(pl);
-        }else{
-            if(landid==-1) return;
-            if(trust(landid,pl.xuid)||landowner(landid,pl.xuid)) FlyOn(pl);
-        }
-    });
+mc.listen('onServerStarted', () => {
+    if (!ll.hasExported('ILAPI_IsPlayerTrusted')) return logger.error('请先安装ILand');
+    let trust = ll.import('ILAPI_IsPlayerTrusted'),
+        landowner = ll.import('ILAPI_IsLandOwner'),
+        getland = ll.import('ILAPI_PosGetLand');
+    let conf = new JsonConfigFile('plugins/iLandFly/config.json');
+    let checkIterval = conf.init('checkInterval', 50)
+    setInterval(() => {
+        mc.getOnlinePlayers().forEach(pl => {
+            if (pl.isCreative || pl.isSpectator) return;
+            let landid = getland(pl.pos);
+            if (pl.canFly) {
+                if (landid == -1) return FlyOff(pl);
+                if (!(trust(landid, pl.xuid) || landowner(landid, pl.xuid))) FlyOff(pl);
+            } else {
+                if (landid == -1) return;
+                if (trust(landid, pl.xuid) || landowner(landid, pl.xuid)) FlyOn(pl);
+            }
+        });
+    }, checkIterval);
 });
